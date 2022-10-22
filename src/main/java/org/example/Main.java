27cd22451b725074a23228d657bffe2271902fde
Ilya <sirllizz@gmail.com>
2022-10-23 00:59:14 +0400

    All Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ import org.example.laba_1.Laba_1;
 
 public class Main {
     public static void main(String[] args) {
-        Laba_1 laba_1 = new Laba_1(0.0001, 0.001, -1, 2);
-        laba_1.dichotomy();
-        laba_1.goldenRatio();
+        Laba_1 laba_1 = new Laba_1(-1, 2);
+        laba_1.dichotomy(0.0001, 0.001);
+        laba_1.goldenRatio(0.001);
+        laba_1.fibonacci(15);
     }
 }
--- src/main/java/org/example/laba_1/Laba_1.java
+++ src/main/java/org/example/laba_1/Laba_1.java
@@ -2,19 +2,15 @@ package org.example.laba_1;
 
 public class Laba_1 {
 
-    private final double eps;
-    private final double delta;
     private final double a;
     private final double b;
 
-    public Laba_1(double eps, double delta, double a, double b) {
-        this.eps = eps;
-        this.delta = delta;
+    public Laba_1(double a, double b) {
         this.a = a;
         this.b = b;
     }
 
-    public void dichotomy(){
+    public void dichotomy(double eps, double delta){
         System.out.println("Dichotomy:\n");
         int counter = 0;
         double xLeft = a;
@@ -35,7 +31,7 @@ public class Laba_1 {
         System.out.println("Iteration: " + counter + "\n");
     }
 
-    public void goldenRatio(){
+    public void goldenRatio(double delta){
         System.out.println("Golden Ratio:\n");
         double xLeft = a;
         double xRight = b;
@@ -60,8 +56,45 @@ public class Laba_1 {
         System.out.println("Iteration: " + counter + "\n");
     }
 
+    public void fibonacci(int n){
+        System.out.println("Fibonacci:\n");
+        double xLeft = a;
+        double xRight = b;
+        double xNearLeft = a + (b - a)*((double)getFibonacciNumber(n-2)/getFibonacciNumber(n));
+        double xNearRight = a + (b - a)*((double)getFibonacciNumber(n-1)/getFibonacciNumber(n));
+        System.out.println("Left: \t" + xLeft + "\tNear Left: \t"+ xNearLeft + " \tNear Right: \t" + xNearRight  + " \tRight: \t" + xRight );
+        do{
+            if(myFunction(xNearLeft)> myFunction(xNearRight)){
+                xLeft = xNearLeft;
+                xNearLeft = xNearRight;
+                xNearRight = xRight - (xNearLeft - xLeft);
+            }else{
+                xRight = xNearRight;
+                xNearRight = xNearLeft;
+                xNearLeft = xLeft + (xRight - xNearRight);
+            }
+            System.out.println("Left: \t" + xLeft + "\tNear Left: \t"+ xNearLeft + " \tNear Right: \t" + xNearRight  + " \tRight: \t" + xRight );
+            n--;
+        }while(n>1);
+        System.out.println("Min: " + (xLeft + xRight) / 2);
+    }
 
     private double myFunction(double x){
         return Math.sinh(2 * x) * Math.sinh(2 * x);
+    }
+
+    private int getFibonacciNumber(int n){
+        if(n<=2){
+            return 1;
+        }else{
+            int previousNum = 1;
+            int nowNum = 1;
+            for (int i = 0; i<n-2;i++){
+                int tmp = nowNum;
+                nowNum +=previousNum;
+                previousNum = tmp;
+            }
+            return nowNum;
+        }
     }
 }
